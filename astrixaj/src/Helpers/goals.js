dc91c60cc37063a546092858e7581b6bb7bfe501
Harsh Gawas <74852649+harshgawas@users.noreply.github.com>
2023-02-11 02:31:16 +0530

    commit

---
+++ astrixaj/src/Helpers/goals.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { API } from "../backend";
+
+export const createObjective = (data) => {
+  return axios
+    .post(`${API}/goal/create-objective`, data, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+      },
+    })
+    .then((res) => {
+      console.log(res.data);
+      if (res.data.success) {
+        toast.success(res.data.message);
+      }
+      return res.data;
+    })
+    .catch((err) => {
+      toast.error(err.response.data.message);
+    });
+};
+
+export const createKeyResults = (data, id) => {
+  console.log(data, id, "data, id");
+  return axios
+    .post(`${API}/goal/create-key-result/${id}`, data, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+      },
+    })
+    .then((res) => {
+      console.log(res.data);
+      if (res.data.success) {
+        toast.success(res.data.message);
+      }
+      return res.data;
+    })
+    .catch((err) => {
+      toast.error(err.response.data.message);
+    });
+};
--- backend/models/Goals.js
+++ backend/models/Goals.js
@@ -1,20 +1,72 @@
 const mongoose = require("mongoose");
 
+const keySchema = new mongoose.Schema({
+  milestone: String,
+  assigned_to: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    default: null,
+  },
+  assigned_to_teams: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Team",
+      default: null,
+    },
+  ],
+  due_date_key: Date,
+  type: {
+    type: String,
+    enum: ["percentage", "number", "currency"],
+  },
+  value: String,
+  target_value: String,
+  linked_to: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "KeyResult",
+    default: null,
+  },
+  created_by_key: {
+    type: String,
+    enum: ["manager", "teamLead", "self"],
+  },
+  created_by_id_key: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+
+    default: null,
+  },
+  created_for_key: {
+    type: String,
+    enum: ["team", "member", "self"],
+  },
+});
+
 const goalSchema = new mongoose.Schema({
   objective: {
     name: String,
     assigned_team: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Team",
+      default: null,
     },
     equal_percentage: Boolean,
     created_by: {
       type: String,
-      enum: ["manager", "teamLead"],
+      enum: ["manager", "teamLead", "self"],
+    },
+    created_by_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    created_for: {
+      type: String,
+      enum: ["team", "member", "self"],
     },
     linked_to: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Goal",
+      default: null,
     },
     target_value: String,
     target_type: {
@@ -26,26 +78,13 @@ const goalSchema = new mongoose.Schema({
   },
   key_results: [
     {
-      milestone: String,
-      assigned_to: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-      assigned_team: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Team",
-      },
-      type: {
-        type: String,
-        enum: ["percentage", "number", "currency"],
-      },
-      value: String,
-      target_value: String,
-      due_date: Date,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "KeyResult",
     },
   ],
 });
 
 const Goal = mongoose.model("Goal", goalSchema);
+const KeyResult = mongoose.model("KeyResult", keySchema);
 
-module.exports = Goal;
+module.exports = { Goal, KeyResult };
